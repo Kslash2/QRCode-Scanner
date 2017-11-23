fb03b0f053e1248a1d6184a029871e9cb0451e3c
Kslash <simoneangeletti93@gmail.com>
2017-11-23 22:07:47 +0100

    First Commit
    First Commit, Android App QRScanner

---
+++ app/src/main/java/com/google/android/gms/samples/vision/barcodereader/MainActivity.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.gms.samples.vision.barcodereader;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
+
+/**
+ * Main activity demonstrating how to pass extra parameters to an activity that
+ * reads barcodes.
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+
+    private static final int RC_BARCODE_CAPTURE = 9001;
+    private static final String TAG = "BarcodeMain";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.read_QR).setOnClickListener(this);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.read_QR) {
+            // launch barcode activity.
+            Intent intent = new Intent(this, BarcodeCaptureActivity.class);
+
+            startActivityForResult(intent, RC_BARCODE_CAPTURE);
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/google/android/gms/samples/vision/barcodereader/Result.java
@@ -0,0 +1,33 @@
+package com.google.android.gms.samples.vision.barcodereader;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Activity that show QRCode Result, in a TextView
+ */
+public class Result extends AppCompatActivity {
+    private String QRCodeReceived="";
+    private TextView resultView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        //getting the result of BarcodeCaptureActivity by intent
+        Intent intent = getIntent();
+        QRCodeReceived = intent.getExtras().getString("qrResult");
+        resultView = (TextView) findViewById(R.id.resultView);
+        resultView.setText(QRCodeReceived);
+
+
+    }
+
+}
